114f98d98d771b8ee22ed782d4d45f750324477a
Mayur Pawar <139702403+mayurpwr7@users.noreply.github.com>
2024-12-04 21:16:32 +0530

    Update stopped_ec2_instance_start_in_region.sh

--- stopped_ec2_instance_start_in_region.sh
+++ stopped_ec2_instance_start_in_region.sh
@@ -7,7 +7,8 @@ REGION="ap-south-1"
 INSTANCE_IDS=$(aws ec2 describe-instances --region $REGION --filters "Name=instance-state-name,Values=stopped" --query "Reservations[*].Instances[*].InstanceId" --output text)
 
 # Start each stopped instance
-for INSTANCE_ID in $INSTANCE_IDS; do
+for INSTANCE_ID in $INSTANCE_IDS; 
+do
   echo "Starting instance $INSTANCE_ID"
   aws ec2 start-instances --region $REGION --instance-ids $INSTANCE_ID
 done
