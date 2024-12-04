4988d4145d46291182998cc22a866dcfbab56f14
Mayur Pawar <139702403+mayurpwr7@users.noreply.github.com>
2024-12-04 21:17:35 +0530

    Update terminate_ec2instance.sh

--- terminate_ec2instance.sh
+++ terminate_ec2instance.sh
@@ -7,7 +7,8 @@ REGION="us-east-2"
 INSTANCE_IDS=$(aws ec2 describe-instances --region $REGION --query "Reservations[*].Instances[*].InstanceId" --output text)
 
 # Check if there are any instances to terminate
-if [ -z "$INSTANCE_IDS" ]; then
+if [ -z "$INSTANCE_IDS" ]; 
+then
   echo "No instances found in region $REGION."
 else
   # Terminate all instances
